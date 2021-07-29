da6cc881d1f685e482277506379215d5298d241b
FirstName LastName <nathangee867@gmail.com>
2021-07-29 08:58:05 -0400

    updated

--- db.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  `postgres://postgres:${process.env.DB_PASS}@${process.env.DB_HOST}${process.env.DB_USER}/`
-);
-module.exports = sequelize;
